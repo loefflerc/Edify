2e93595a2c11e943509e9f1b0749be006b2c7e32
IrishAdo <irishado@php-programmers.net>
2013-02-01 20:19:38 +0000

    A basic couple of Utilitiy classes.
    Utils/Log will allow you to write debug statements that will output to the php error log rather than the application output.
    
    Utils/Loader a basic class auto loader that will allow developers to register their frameworks with this autoloader.

---
+++ Utils/Loader.php
@@ -0,0 +1,117 @@
+<?php
+
+/** The Loader class which is part of the Edify\Utils namespace
+ * 
+ */
+
+namespace Edify\Utils;
+
+
+// side effect: loads  the Utils/Log class as it is needed between 
+require_once PP_NET_CL_PATH . "/Utils/Log.php";
+
+/** Define the Loader class that allows autoloading of classes with out have to
+ * include each and every class on every script.
+ * 
+ * This allows you to not worry about including a class as long as the class file 
+ * follows the Standard for namespaces <vender>/path/of/class.php
+ * 
+ * @author IrishAdo <irishado@php-programmers.net>
+ * @licence Freetard - do what you wish technology, give back, keep, sell up to you.
+ */
+class Loader {
+
+    // define a instance variable
+    /**
+     *
+     * @var SELF the current instance
+     */
+    private static $instance;
+
+    /**
+     * @static
+     * @var Array list of vendors that the autoloader knows about.
+     */
+    private static $vendors;
+
+    /** initalise the class and add the Path to this Library Vendor
+     * 
+     * @author IrishAdo <irishado@php-programmers.net>
+     */
+    public static function init($basePath) {
+        if (self::$instance == NULL) {
+            self::$instance = new self();
+        }
+        
+        self::registerVendor('Edify', $basePath);
+        return self::$instance;
+    }
+
+    /** Constructor which sets the loader function to be an auto loader
+     *
+     * @author IrishAdo <irishado@php-programmers.net>
+     */
+    public function __construct() {
+        spl_autoload_register(array($this, "auto"));
+    }
+
+    /** Preprocessor function which will attempt to include a file contains a class before the new command executes
+     * 
+     * @author IrishAdo <irishado@php-programmers.net>
+     * @param Class name 
+     */
+    public function auto($class) {
+        \Edify\Utils\Log::debugLog("[Edify\Utils\Loader]", "class name = $class");
+        // split the namespace and class up into parts
+        $parts = explode("\\", $class);
+
+        // the first part of the namespace is the vendor a unique project identifier
+        $choosenVendor = $parts[0];
+
+        // remove the end and the beginning of the path
+        unset($parts[0]);
+
+        $path = implode("/", $parts);
+
+        // if we have not listed the path for that vendor then we don't know where it is so die.
+        if (!isset(self::$vendors[$choosenVendor])) {
+            \Edify\Utils\Log::debugError("[Edify\Utils\Loader]", "Vendor $choosenVendor not found use \Edify\Utils\Loader::AddVendorPath(\$vendor,\$path);");
+            throw new \Exception('Sorry we could not find a class located at '. $checkPathforFile);
+        } else {
+
+            // work out the path to look for the file based on the Vendors path.
+            $checkPathforFile = realpath(self::$vendors[$choosenVendor]) . "/" . $path . ".php";
+
+            // if the file does exist then include it or die.
+            if (file_exists($checkPathforFile)) {
+                \Edify\Utils\Log::debugLog("[Edify\Utils\Loader]", "Found -> $checkPathforFile");
+                require_once($checkPathforFile);
+            } else {
+                \Edify\Utils\Log::debugError("[Edify\Utils\Loader]", "Found -> $checkPathforFile");
+                throw new \Exception('Sorry we could not find a class located at  '. $checkPathforFile);
+            }
+        }
+    }
+
+    /** function to register a vendor to the Auto loader so that it knows where
+     * subdirectory the files for that vendor is located.
+     *
+     * For example if I created a new application called Mapper then the
+     * Namespace for the elements in Mapper should be Mapper\*  by registering
+     * the vendor "Mapper" we can look in the correct location for all of the
+     * Classes for that namespace.  This allows developers to have many different
+     * namespaces and for the Autoloader functionality to know where to go for each.
+     *
+     * @author IrishAdo <irishado@php-programmers.net>
+     * @param String the name of the Vendor
+     * @param String Path to look for the class file.
+     * @example self::AddVendorPath('Edify', PP_NET_CL_PATH);
+     */
+    public static function registerVendor($vendor, $path) {
+        self::$vendors[$vendor] = $path;
+    }
+
+}
+
+\Edify\Utils\Loader::init(PP_NET_CL_PATH);
+?>
---
+++ Utils/Log.php
@@ -0,0 +1,93 @@
+<?php
+
+/** The Log class which is part of the Edify\Utils namespace
+ * 
+ */
+
+namespace Edify\Utils;
+
+/** Define the Log class
+ * 
+ * This allows you to log information into the error_log file
+ * @author IrishAdo <irishado@php-programmers.net>
+ * @licence Freetard - do what you wish technology, give back, keep, sell up to you.
+ */
+class Log {
+
+    /** The level at whcih the debugger will actually write to the error_log
+     * @static
+     * @var String a list of the handles that are listed to output to the error_log
+     */
+    private static $debugLevel = "";
+
+    /** the instance of this object so that if you initialise another class of this type it just uses this one.
+     * @static
+     * @var Self an Instance of this class
+     */
+    private static $instance;
+
+    /** Initialise the single instance
+     * @author IrishAdo <irishado@php-programmers.net>
+     */
+    public static function init() {
+        if (self::$instance == NULL) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    /** Constructor for this class
+     * @author IrishAdo <irishado@php-programmers.net>
+     */
+    public function __construct() {
+
+    }
+
+    /** Allow the setting of the Debug level this will allow you to specify the error handles that will be reported in the error log.
+     * @param String Handle to allow to debug.
+     * @author IrishAdo <irishado@php-programmers.net>
+     */
+    static function setDebugLevel($handleString) {
+        self::$debugLevel = $handleString;
+    }
+
+    /** add a statement to the error log if the $handle is listed in the debugLevel
+     * 
+     * @parma String handle to debug
+     * @param String statement to write ot the log.
+     * @author IrishAdo <irishado@php-programmers.net>
+     * @example /Utils/Log::debugLog("Database", $sqlStatement);
+     */
+    static function debugLog($handle, $statement) {
+        if (
+        // if debug is ALL then write message to error_log file
+            self::$debugLevel == "ALL"
+            ||
+            in_array(
+                // strip the square brackets off the handle
+                str_replace(Array("[", "]"), Array("", ""), $handle),
+                // and check it in not in this array of handles
+                explode(",", self::$debugLevel . ",")
+            )
+        ) {
+            // write to the error_log rather than the screen to keep information secure
+            error_log("$handle: $statement\n");
+        }
+    }
+
+    /** add a statement to the error log unconditionaly
+     * 
+     * @parma String handle to debug
+     * @param String statement to write ot the log.
+     * @author IrishAdo <irishado@php-programmers.net>
+     * @example /Utils/Log::debugError("Database", "Connection failed!");
+     */
+    static function debugError($handle, $statement) {
+        error_log("$handle: $statement\n");
+    }
+
+}
+
+// side effect: initialises the class as a singleton.
+\Edify\Utils\Log::init();
+?>
