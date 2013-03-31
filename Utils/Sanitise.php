fda97c7ba4bb5d064d011b9958b59da5377cfa6c
IrishAdo <irishado@php-programmers.net>
2013-03-31 13:29:24 +0100

    Added new function isReal and made the names of the functions correct (doh wasn't thinking) also made the isNatural function actually return the value as an Integer

--- Utils/Sanitise.php
+++ Utils/Sanitise.php
@@ -21,19 +21,19 @@ class Sanitise {
      *
      * @param type $val
      * @param type $acceptzero
-     * @return boolean
+     * @return int
      * @author ja at krystof dot org (php.net manual)
      */
-    function is_natural($val, $acceptzero = false) {
+    function isNatural($val, $acceptzero = false) {
         $return = ((string) $val === (string) (int) $val);
         if ($acceptzero)
             $base = 0;
         else
             $base = 1;
         if (!$return || intval($val) < $base){
-            throw new \Edify\Exceptions\Sanitise("Value supplied is not a natural whole number - 00000003");
+            throw new \Edify\Exceptions\Sanitise("Value supplied is not a natural whole number - 00000001");
         }
-        return $return;
+        return (int) $val;
     }
 
     /**
@@ -44,16 +44,37 @@ class Sanitise {
      * @param type $maxLength
      * @return String
      */
-    function is_text($val, $maxLength = -1) {
+    function isText($val, $maxLength = -1) {
         if (!is_string($val)) {
             throw new \Edify\Exceptions\Sanitise("Value supplied is not a String - 00000001");
         }
         if ($maxLength!=-1 && strlen($val) > $maxLength) {
             throw new \Edify\Exceptions\Sanitise("Value supplied is not a String - 00000002");
         }
-        return $val;
+        return (string) $val;
     }
 
+    /**
+     * Is the parameter a real number
+     *
+     * A real number can have decimal places but no punctation or symbols
+     *
+     * @param type $val
+     * @return float
+     * @author ja at krystof dot org (php.net manual)
+     */
+    function isReal($val) {
+        $return = ((string) $val === (string) (float) ($val));
+
+        if($val!== "0" && floatval($val)==(float)0){
+            throw new \Edify\Exceptions\Sanitise("Value supplied is not a real number - 00000001");
+        }
+        // the example 123.40 when passed to floatval becomes 123.4 
+        if (!$return && floatval($val)==(float)0){
+            throw new \Edify\Exceptions\Sanitise("Value supplied is not a real number - 00000002");
+        }
+        return (float) $val;
+    }
 }
 
 ?>
