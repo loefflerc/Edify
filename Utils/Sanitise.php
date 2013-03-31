a4022ae6b353519dc1b5c4a0b6060806bd2acd53
IrishAdo <irishado@php-programmers.net>
2013-03-31 13:55:44 +0100

    added a isGUID function to the Sanitise class

--- Utils/Sanitise.php
+++ Utils/Sanitise.php
@@ -51,6 +51,9 @@ class Sanitise {
         if ($maxLength!=-1 && strlen($val) > $maxLength) {
             throw new \Edify\Exceptions\Sanitise("Value supplied is not a String - 00000002");
         }
+        if ($val === (string) $val) {
+            throw new \Edify\Exceptions\Sanitise("Value supplied is not a String - 00000003");
+        }
         return (string) $val;
     }
 
@@ -69,11 +72,34 @@ class Sanitise {
         if($val!== "0" && floatval($val)==(float)0){
             throw new \Edify\Exceptions\Sanitise("Value supplied is not a real number - 00000001");
         }
-        // the example 123.40 when passed to floatval becomes 123.4 
+        // the example 123.40 when passed to floatval becomes 123.4
         if (!$return && floatval($val)==(float)0){
             throw new \Edify\Exceptions\Sanitise("Value supplied is not a real number - 00000002");
         }
         return (float) $val;
+    }
+
+    /**
+     * Is the parameter a GUID.
+     *
+     * A GUID number is a HEXIDECIMAL number made up of 5 parts seperated by hypens
+     *
+     * a 8 character String
+     * a 4 character String
+     * a 4 character String
+     * a 4 character String
+     * a 12 character String
+     *
+     * @param type $val
+     * @return float
+     * @author ja at krystof dot org (php.net manual)
+     */
+    function isGUID($val) {
+        if(preg_match('/([0-9A-F]{8})\-([0-9A-F]{4})\-([0-9A-F]{4})\-([0-9A-F]{4})\-([0-9A-F]{12})/', $val)===false){
+            throw new \Edify\Exceptions\Sanitise("Value supplied is not a GUID - 00000001");
+        }
+
+        return (string) $val;
     }
 }
 
